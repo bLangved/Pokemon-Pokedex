0c528bb8571260c70a7956dcb4a0adc2dce5f2e3
Bjørnar H. Langved <b.langved@icloud.com>
2024-02-01 17:19:27 +0100

    new bootstrap navbar v.1

---
+++ .netlify/functions-internal/ssr/chunks/index_1m2TO4tB.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/index_G3-rpqHA.mjs').then(n => n.i);
+
+export { page };
