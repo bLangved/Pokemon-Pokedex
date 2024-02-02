39d42cd53470047310576bfeec933a5d653ca70c
Bjørnar H. Langved <b.langved@icloud.com>
2024-02-02 11:33:22 +0100

    new bootstrap navbar v.2
    - Added some scripts for handling dark/light color schemes (for nav, and rest of the site).
    - Removed some redundant scss styling
    - Removed the toggleTheme, and changed it to root variables
    - Added a new v1. logo to the header
    - Removed some redundant logos/images

--- .netlify/functions-internal/ssr/chunks/generic_SAArqWRC.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/generic_ubuDuR6U.mjs');
-
-export { page };
