b1026a8fad40c43ea1c1561367c773bb5ce1dafb
Bjørnar H. Langved <b.langved@icloud.com>
2024-02-08 18:45:28 +0100

    Little uncertain what these are (except netlify build-stuff). They came up after I ran "run npm build" after I was done with the branch, and was commiting to the existing PR.

--- .netlify/functions-internal/ssr/chunks/index_e2gq0_g0.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/index_jT7CdQ8n.mjs').then(n => n.a);
-
-export { page };
