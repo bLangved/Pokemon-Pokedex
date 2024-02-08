b87c0d7dde729e3e0d08782aab2b66278c47aa1c
Michael River Nilsson <mrn@michaelriver.dev>
2024-02-08 16:11:16 +0100

    Changes the loader to be a spinning pokeball

--- src/js/components/loadingAnimation.js
+++ src/js/components/loadingAnimation.js
@@ -2,9 +2,9 @@ const loading = document.querySelector('.loadingContainer');
 
 // Function to show the loading animation
 export function showLoadingAnimation() {
-  loading.style.display = 'block';
+  loading.style.display = 'flex';
 }
 // Function to hide the loading animation
 export function hideLoadingAnimation() {
-  loading.style.display = 'none';
+  // loading.style.display = 'none';
 }
